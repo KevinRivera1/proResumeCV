333ed1f506c2d6292514e326816386ee597c1f27
KevinRivera1 <shimpo11.kr@gmail.com>
2025-05-08 14:48:44 -0500

    [ADD]: Add Initial Structure

---
+++ src/features/curriculum/presentational/FormResumeCv.tsx
@@ -0,0 +1,12 @@
+
+export const FormResumeCv = () =>{
+    return (
+        <>
+            <form className="bg-yellow-400">
+                <header>
+                    <h2>Formulario ResumeCV</h2>
+                </header>
+            </form>
+        </>
+    )
+}
