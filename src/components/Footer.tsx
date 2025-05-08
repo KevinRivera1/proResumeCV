b0273b09662966f0bd2f0e0c67d46cd34ee33228
KevinRivera1 <shimpo11.kr@gmail.com>
2025-05-08 18:27:07 -0500

    [FORMAT]: format code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 export const Footer = () => {
-    return (
-        <footer>
-            <div className="container mx-auto">
-                <span> &copy; 2025 | Realizado Por Kevin Rivera</span>
-            </div>
-        </footer>
-    )
-}
+	return (
+		<footer>
+			<div className="container mx-auto">
+				<span> &copy; 2025 | Realizado Por Kevin Rivera</span>
+			</div>
+		</footer>
+	);
+};
